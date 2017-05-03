abbfaf46be3c7eb6e4d21c243d2472ce33dd80e7
Vladislav Ezerski <vladanchik97@mail.ru>
2017-05-03 03:49:32 +0300

    Added the ability to take photos from the camera

--- app/src/main/java/com/example/vladislavezerski/geonotes/NotesAddActivity.java
+++ app/src/main/java/com/example/vladislavezerski/geonotes/NotesAddActivity.java
@@ -1,36 +1,71 @@
 package com.example.vladislavezerski.geonotes;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static android.location.LocationManager.NETWORK_PROVIDER;
 
 public class NotesAddActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1;
+    private static final int REQUEST_CODE_PHOTO = 2;
+    private static final int REQUEST_CODE_MAP = 3;
+
+    final String TAG = "myLogs";
+
+    private MapView mapViewMini;
+
     private ImageView newNote;
     private ImageView changeColor;
     private EditText txtBody;
     private TextView otmena;
-    private Button btnMap;
+    private ImageView btnMap;
+    private ImageView btnCamera;
+    private ImageView ivPhoto;
+    private GoogleMap googleMap;
+    private Bundle savedInstanceState;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.savedInstanceState = savedInstanceState;
         setContentView(R.layout.notes_add);
         newNote = (ImageView) findViewById(R.id.imageView2);
         changeColor = (ImageView) findViewById(R.id.imgChangeColor);
         txtBody = (EditText) findViewById(R.id.txt_notes_body);
         otmena = (TextView) findViewById(R.id.txt_otmena);
-        btnMap = (Button) findViewById(R.id.btn_map);
+        btnMap = (ImageView) findViewById(R.id.btn_map);
+        btnCamera = (ImageView) findViewById(R.id.btn_camera);
+        ivPhoto = (ImageView) findViewById(R.id.ivPhoto);
+        mapViewMini = (MapView) findViewById(R.id.mapViewMini);
+
+
+        initMap(savedInstanceState);
+
 
         changeColor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +78,7 @@ public class NotesAddActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.putExtra("text",txtBody.getText().toString());
+                intent.putExtra("text", txtBody.getText().toString());
                 setResult(RESULT_OK, intent);
                 finish();
             }
@@ -59,10 +94,46 @@ public class NotesAddActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MapActivity.class);
-                startActivity(intent);         }
+                startActivity(intent);
+            }
+        });
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE_PHOTO);
+            }
         });
     }
 
+    private void initMap(@Nullable Bundle savedInstanceState) {
+        mapViewMini.onCreate(savedInstanceState);
+        mapViewMini.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap map) {
+                googleMap = map;
+                googleMap.getUiSettings().setZoomControlsEnabled(true);
+                googleMap.setMyLocationEnabled(true);
+
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                Location location = locationManager.getLastKnownLocation(NETWORK_PROVIDER);
+
+                if (location != null) {
+
+                    googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.builder()
+                            .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                            .zoom(10f).build()));
+                }
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        googleMap.addMarker(new MarkerOptions().position(latLng).title("MyPosition"));
+                    }
+                });
+            }
+        });
+    }
 
 
     @Override
@@ -73,10 +144,82 @@ public class NotesAddActivity extends AppCompatActivity {
             changeColor.getBackground().setColorFilter(data.getIntExtra("color", Color.BLACK), PorterDuff.Mode.SRC_ATOP);
         }
 
+        if (requestCode == REQUEST_CODE_MAP) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+
+                }
+            }
+        }
+
+        if (requestCode == REQUEST_CODE_PHOTO) {
+            if (resultCode == RESULT_OK) {
+                if (data == null) {
+                    Log.d(TAG, "Intent is null");
+                } else {
+                    Log.d(TAG, "Photo uri: " + data.getData());
+                    Bundle bndl = data.getExtras();
+                    if (bndl != null) {
+                        Object obj = data.getExtras().get("data");
+                        if (obj instanceof Bitmap) {
+                            Bitmap bitmap = (Bitmap) obj;
+                            Log.d(TAG, "bitmap " + bitmap.getWidth() + " x "
+                                    + bitmap.getHeight());
+                            ivPhoto.setImageBitmap(bitmap);
+                        }
+                    }
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d(TAG, "Canceled");
+            }
+        }
+
     }
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return false;
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mapViewMini != null) {
+            mapViewMini.onStart();
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mapViewMini != null) {
+            mapViewMini.onStop();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mapViewMini != null) {
+            mapViewMini.onResume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mapViewMini != null) {
+            mapViewMini.onDestroy();
+        }
+//        googleMap.setMyLocationEnabled(false);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if (mapViewMini != null) {
+            mapViewMini.onLowMemory();
+        }
+    }
 }
